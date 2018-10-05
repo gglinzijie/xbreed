0c32b1ae12f4883adaeab76190f7f4652a52d83f
gglinzijie <295501332@qq.com>
2018-10-05 17:32:51 +0900

    Update Sample_hp_v9_Commented.R

--- R/Sample_hp_v9_Commented.R
+++ R/Sample_hp_v9_Commented.R
@@ -967,11 +967,13 @@ counter_id<- sum(my)
 id1 <- (counter_id+1)
 id2 <- (counter_id+x)
 id  <- id1:id2
-if(No_off_per_sire==floor(No_off_per_sire)){
-sire <- rep(sires,each=No_off_per_sire)
-} else
-sire<-sample(sires,x,replace=TRUE)
-dam <- rep(dams,each=No_off_per_dam)
+#if(No_off_per_sire==floor(No_off_per_sire)){
+#sire <- rep(sires,each=No_off_per_sire)
+#} else
+#sire<-sample(sires,x,replace=TRUE)
+#dam <- rep(dams,each=No_off_per_dam)
+sire<-rep(sire, each=(No_off_per_sire*length(dams)))
+dam<-rep(rep(dam,each=No_off_per_dam),length(sires))
 generation <-gene_counter +1 
 sex <-sample(c('F','M'),x,replace=T)
 env <-rnorm(x,mean=0,sd=sqrt(vv))
