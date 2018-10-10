22e3c264f3d9e1a6ab764c34ecab881f1ed94ff3
gglinzijie <295501332@qq.com>
2018-10-10 16:20:29 +0900

    Update Sample_hp_v9_Commented.R

--- R/Sample_hp_v9_Commented.R
+++ R/Sample_hp_v9_Commented.R
@@ -390,11 +390,11 @@ cat('Controlling input data ...',fill=TRUE)
 		if(floor(tra_size)!=tra_size | tra_size<1){
 		stop('\n','---size in argument "Training" should be positive integer.')
 		}
-		if(tra_size>(litter_size*Selection[1,1]*Selection[1,2])){
-		avai<-litter_size*Selection[1,1]*Selection[1,2]
-		cat('\n','---Training size:',tra_size,' in argument "Training" is grater than number of available individuals:',avai,fill=TRUE)
-		stop("\n",'Solution 1: Increase litter size.',"\n",'Solution 2: Decrease size of training.',"\n",'Solution 3: Increase number of dmas selected.')		
-		}
+		#if(tra_size>(litter_size*Selection[1,1]*Selection[1,2])){
+		#avai<-litter_size*Selection[1,1]*Selection[1,2]
+		#cat('\n','---Training size:',tra_size,' in argument "Training" is grater than number of available individuals:',avai,fill=TRUE)
+		#stop("\n",'Solution 1: Increase litter size.',"\n",'Solution 2: Decrease size of training.',"\n",'Solution 3: Increase number of dmas selected.')		
+		#}
 		
 		if(tra_size>(litter_size*Female_founders[1]*Male_founders[1])){
 		avai<-litter_size*as.numeric(Female_founders[1])*as.numeric(Male_founders[1])
